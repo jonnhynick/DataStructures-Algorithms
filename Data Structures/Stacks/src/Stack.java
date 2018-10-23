acbd1443b9d2707a45387dc71b98999df99a7143
jonnhynick <nickolai.mtz@icloud.com>
2018-10-22 22:14:18 -0700

    Stack Data Structure
    Implementation of the stack data structure with simple peek, pop and push methods.

---
+++ Data Structures/Stacks/src/Stack.java
@@ -0,0 +1,55 @@
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.NoSuchElementException;
+
+public class Stack {
+
+    private Node top;
+    private int size;
+    private class Node{
+        public int data;
+        public Node link;
+
+        public Node(int data){
+            this.data = data;
+            link = null;
+        }
+    }
+
+    public Stack(){
+        top = null;
+        size = 0;
+    }
+
+    public void push(int data){
+        Node _newNode = new Node(data);
+        if(top == null)
+            top = _newNode;
+        else{
+            _newNode.link = top;
+            top = _newNode;
+        }
+        this.size++;
+    }
+
+    public int pop(){
+        if(top == null)
+            throw new NoSuchElementException();
+        else{
+            Node _topNode = top;
+            top = _topNode.link;
+            size--;
+            return _topNode.data;
+        }
+    }
+
+    public int peek(){
+        if(top == null)
+            throw new NoSuchElementException();
+        else{
+            return top.data;
+        }
+    }
+
+}
