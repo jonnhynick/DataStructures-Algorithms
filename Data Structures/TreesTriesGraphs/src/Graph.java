932b3f163c9c3061c0156d3779a09e1ab7cf43c6
jonnhynick <nickolai.mtz@icloud.com>
2018-10-23 21:57:33 -0700

    Tries and Graph Structures Implemented
    Graph implemented as a Adjacency matrix.
    Tries implemented using HashMaps

--- Data Structures/TreesTriesGraphs/src/Graph.java
+++ Data Structures/TreesTriesGraphs/src/Graph.java
@@ -1,2 +1,47 @@
+import java.util.LinkedList;
+/*
+*Graphs can be represented in either adjacency lists
+* or adjacency matrices.
+* In this example we will use adjacency lists which take less space than a matrix.
+* */
 public class Graph {
+    private int vertices;
+    private LinkedList<Integer> adjacencyList[];
+
+    public Graph(int vertices){
+        this.vertices = vertices;
+
+        adjacencyList = new LinkedList[vertices];
+        //Initialize all LinkedLists inside the array.
+        for(int i = 0; i < vertices; i++){
+            adjacencyList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int source, int destination){
+        //check if either source or destination is an invalid vertex
+        if(source > vertices - 1 || destination > vertices -1)
+            return;
+        else{
+            //add from source to destination
+            adjacencyList[source].add(destination);
+
+            //if graph is undirected then add from destination to source.
+            adjacencyList[destination].add(source);
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        for(int i =0; i < vertices; i++){
+            str.append("Vertex " + i + ": ");
+            for(Integer val : adjacencyList[i]){
+                str.append(val + " ");
+            }
+            str.append("\n");
+        }
+
+        return str.toString();
+    }
 }
--- Data Structures/TreesTriesGraphs/src/Tries.java
+++ Data Structures/TreesTriesGraphs/src/Tries.java
@@ -1,2 +1,82 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tries {
+    private TrieNode root;
+
+    public Tries() {
+        root = new TrieNode();
+    }
+
+    void insert(String word) {
+        TrieNode current = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            current = current.getChildren().computeIfAbsent(word.charAt(i), c -> new TrieNode());
+        }
+        current.setEndOfWord(true);
+    }
+
+    boolean delete(String word) {
+        return delete(root, word, 0);
+    }
+
+    boolean containsNode(String word) {
+        TrieNode current = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            TrieNode node = current.getChildren().get(ch);
+            if (node == null) {
+                return false;
+            }
+            current = node;
+        }
+        return current.isEndOfWord();
+    }
+
+    boolean isEmpty() {
+        return root == null;
+    }
+
+    private boolean delete(TrieNode current, String word, int index) {
+        if (index == word.length()) {
+            if (!current.isEndOfWord()) {
+                return false;
+            }
+            current.setEndOfWord(false);
+            return current.getChildren().isEmpty();
+        }
+        char ch = word.charAt(index);
+        TrieNode node = current.getChildren().get(ch);
+        if (node == null) {
+            return false;
+        }
+        boolean shouldDeleteCurrentNode = delete(node, word, index + 1) && !node.isEndOfWord();
+
+        if (shouldDeleteCurrentNode) {
+            current.getChildren().remove(ch);
+            return current.getChildren().isEmpty();
+        }
+        return false;
+    }
+
+    private class TrieNode {
+        private final Map<Character, TrieNode> children = new HashMap<>();
+        private boolean endOfWord;
+
+        Map<Character, TrieNode> getChildren() {
+            return children;
+        }
+
+        boolean isEndOfWord() {
+            return endOfWord;
+        }
+
+        void setEndOfWord(boolean endOfWord) {
+            this.endOfWord = endOfWord;
+        }
+    }
 }
+
+
