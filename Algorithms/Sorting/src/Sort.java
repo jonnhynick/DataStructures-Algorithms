a13b69f631b2da7087a78193273cb67aa4543b5b
jonnhynick <nickolai.mtz@icloud.com>
2018-10-24 17:13:16 -0700

    Quick Sort, Merge Sort, Radix Sort, Selection Sort and Bubble Sort
    Implementations for each, both Selection and Bubble Sort where implemented as methods since they are not as complex to implement.

---
+++ Algorithms/Sorting/src/Sort.java
@@ -0,0 +1,211 @@
+import java.util.Arrays;
+
+public class Sort {
+    //Time complexity O(nlog(n))
+    //Space complexity O(1)
+    class quickSort {
+
+        public int partition(int arr[], int low, int high) {
+            int pivot = arr[high];
+            int i = (low - 1); // index of smaller element
+            for (int j = low; j < high; j++) {
+                // If current element is smaller than or
+                // equal to pivot
+                if (arr[j] <= pivot) {
+                    i++;
+
+                    // swap arr[i] and arr[j]
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            // swap arr[i+1] and arr[high] (or pivot)
+            int temp = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp;
+
+            return i + 1;
+        }
+
+        public void sort(int arr[], int low, int high) {
+            if (low < high) {
+            /* pi is partitioning index, arr[pi] is
+              now at right place */
+                int pi = partition(arr, low, high);
+
+                // Recursively sort elements before
+                // partition and after partition
+                sort(arr, low, pi - 1);
+                sort(arr, pi + 1, high);
+            }
+        }
+
+        public void printArray(int arr[]) {
+            int n = arr.length;
+            for (int i = 0; i < n; ++i)
+                System.out.print(arr[i] + " ");
+            System.out.println();
+        }
+    }
+    //Time complexity O(nlog(n))
+    //Space complexity O(n)
+    class mergeSort {
+
+        public void merge(int arr[], int l, int m, int r) {
+            // Find sizes of two subarrays to be merged
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            /* Create temp arrays */
+            int L[] = new int[n1];
+            int R[] = new int[n2];
+
+            /*Copy data to temp arrays*/
+            for (int i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (int j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+
+            /* Merge the temp arrays */
+
+            // Initial indexes of first and second subarrays
+            int i = 0, j = 0;
+
+            // Initial index of merged subarry array
+            int k = l;
+            while (i < n1 && j < n2) {
+                if (L[i] <= R[j]) {
+                    arr[k] = L[i];
+                    i++;
+                } else {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            /* Copy remaining elements of L[] if any */
+            while (i < n1) {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            /* Copy remaining elements of R[] if any */
+            while (j < n2) {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        // Main function that sorts arr[l..r] using
+        // merge()
+        public void sort(int arr[], int l, int r) {
+            if (l < r) {
+                // Find the middle point
+                int m = (l + r) / 2;
+
+                // Sort first and second halves
+                sort(arr, l, m);
+                sort(arr, m + 1, r);
+
+                // Merge the sorted halves
+                merge(arr, l, m, r);
+            }
+        }
+
+        /* A utility function to print array of size n */
+        public void printArray(int arr[]) {
+            int n = arr.length;
+            for (int i = 0; i < n; ++i)
+                System.out.print(arr[i] + " ");
+            System.out.println();
+        }
+    }
+    //Time complexity O(n)
+    //Space complexity O(1)
+    class radixSort {
+
+        public int getMax(int arr[], int n) {
+            int mx = arr[0];
+            for (int i = 1; i < n; i++)
+                if (arr[i] > mx)
+                    mx = arr[i];
+            return mx;
+        }
+
+        public void countSort(int arr[], int n, int exp) {
+            int output[] = new int[n]; // output array
+            int i;
+            int count[] = new int[10];
+            Arrays.fill(count, 0);
+
+            // Store count of occurrences in count[]
+            for (i = 0; i < n; i++)
+                count[(arr[i] / exp) % 10]++;
+
+            // Change count[i] so that count[i] now contains
+            // actual position of this digit in output[]
+            for (i = 1; i < 10; i++)
+                count[i] += count[i - 1];
+
+            // Build the output array
+            for (i = n - 1; i >= 0; i--) {
+                output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+                count[(arr[i] / exp) % 10]--;
+            }
+
+            // Copy the output array to arr[], so that arr[] now
+            // contains sorted numbers according to curent digit
+            for (i = 0; i < n; i++)
+                arr[i] = output[i];
+        }
+
+        public void radixsort(int arr[], int n) {
+            // Find the maximum number to know number of digits
+            int m = getMax(arr, n);
+
+            // Do counting sort for every digit. Note that instead
+            // of passing digit number, exp is passed. exp is 10^i
+            // where i is current digit number
+            for (int exp = 1; m / exp > 0; exp *= 10)
+                countSort(arr, n, exp);
+        }
+    }
+    //Time complexity O(n^2)
+    //Space complexity O(1)
+    public void selectionSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    //Time complexity O(n^2)
+    //Space Complexity O(1)
+    public void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+}
